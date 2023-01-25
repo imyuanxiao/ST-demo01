fa1df11e3d49129250b2e2d4efbcd7b3dcc8bc35
13524342068 <Gitee19951226>
2023-01-25 17:41:44 +0100

    123

---
+++ test/c.java
@@ -0,0 +1,10 @@
+package test;
+
+public class c {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("spring");
+    }
+
+}
